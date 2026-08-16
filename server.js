63f72b28e0998016a7a7fa2e7063340f5c1831dc
ahmadrazi0077-maker <ahmadrazi0077@gmail.com>
2026-08-16 23:00:36 +0500

    Fix CORS - allow all origins

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ const app = express();
 
 // CORS
 app.use(cors({
-  origin: ['https://civil-estimators.vercel.app/', 'http://127.0.0.1:3000'],
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
 
